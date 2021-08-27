3947296ab5041eb96a9e5b512f8cced94523742d
HoangHuuy <hoanghuy020420@gmail.com>
2021-08-27 18:58:02 +0700

    sua file welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Trang chủ</a>
                     @else
                         <a href="{{ route('login') }}">Đăng nhập</a>
 
